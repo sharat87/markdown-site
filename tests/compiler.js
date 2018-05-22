ee36e81dc29145fd9f1444687e031b0639d9197d
sharat87 <shrikantsharat.k@gmail.com>
2018-05-22 12:09:11 +0530

    Some more working and passing tests.

--- tests/compiler.js
+++ tests/compiler.js
@@ -1,6 +1,18 @@
-describe('Compiler', function () {
-    it("should render a para", function () {
-        const {meta, html} = Compiler.compile('abc');
-        expect(html).toEqual('<p>abc</p>');
+describe('Compiler', () => {
+    it('should render a single paragraph', function () {
+        expect(Compiler.compile('abc').box.innerHTML).toEqual('<p>abc</p>');
+    });
+
+    it('should render replacements', () => {
+        expect(toHTML('abc -> def')).toEqual('<p>abc \u2192 def</p>');
+    });
+
+    it('should apply replacements within emphasis', () => {
+        expect(toHTML('abc *def -> hij* klm')).toEqual('<p>abc <em>def \u2192 hij</em> klm</p>');
     });
 });
+
+
+function toHTML(md) {
+    return Compiler.compile(md).box.innerHTML;
+}
